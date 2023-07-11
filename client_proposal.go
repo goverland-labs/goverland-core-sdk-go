2037c8828d9482fa2726f3cf826ed5952b48f7c5
Dmitry <dmitry@goverland.xyz>
2023-07-11 16:38:44 +0300

    Add proposal top endpoint

--- client_proposal.go
+++ client_proposal.go
@@ -74,6 +74,40 @@ type GetProposalVotesRequest struct {
 	Limit  int
 }
 
+type GetProposalTopRequest struct {
+	Offset int
+	Limit  int
+}
+
+func (c *Client) GetProposalTop(ctx context.Context, params GetProposalTopRequest) (*proposal.List, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/proposals/top", c.baseURL), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	if params.Offset != 0 {
+		q.Add("offset", strconv.Itoa(params.Offset))
+	}
+	if params.Limit != 0 {
+		q.Add("limit", strconv.Itoa(params.Limit))
+	}
+	req.URL.RawQuery = q.Encode()
+
+	var result []proposal.Proposal
+	headers, err := c.sendRequest(ctx, req, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proposal.List{
+		Items:    result,
+		Offset:   GetOffsetFromHeaders(headers),
+		Limit:    GetLimitFromHeaders(headers),
+		TotalCnt: GetTotalCntFromHeaders(headers),
+	}, nil
+}
+
 func (c *Client) GetProposalVotes(ctx context.Context, id string, params GetProposalVotesRequest) (*proposal.VoteList, error) {
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/proposals/%s/votes", c.baseURL, id), nil)
 	if err != nil {
