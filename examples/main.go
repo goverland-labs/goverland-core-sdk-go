206b318ae53396ddab3bfe4ef03543c12fd5ce6e
Dmitry <dmitry@goverland.xyz>
2023-08-22 16:58:35 +0300

    Add proposal timeline field

--- examples/main.go
+++ examples/main.go
@@ -9,6 +9,5 @@ const defaultBaseURL = "http://localhost:88/v1/"
 var defaultSubscriberID = uuid.MustParse("b3be82c6-7ccc-4db6-9506-4d0c9c36ab76")
 
 func main() {
-	//createSubscriber()
 	updateSubscriber()
 }
