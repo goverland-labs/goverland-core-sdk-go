ccc643ea0cb0ada87e2b47ff4c09dbaa11521892
Dmitry <dmitry@goverland.xyz>
2023-09-05 15:55:44 +0300

    Mark votes choice field as json.RawMessage due to multiple values

--- proposal/vote.go
+++ proposal/vote.go
@@ -1,20 +1,22 @@
 package proposal
 
 import (
+	"encoding/json"
+
 	"github.com/google/uuid"
 )
 
 type Vote struct {
-	ID                    string    `json:"id"`
-	Ipfs                  string    `json:"ipfs"`
-	Voter                 string    `json:"voter"`
-	DaoID                 uuid.UUID `json:"dao_id"`
-	ProposalID            string    `json:"proposal_id"`
-	Choice                int       `json:"choice"`
-	Created               uint64    `json:"created"`
-	Reason                string    `json:"reason"`
-	App                   string    `json:"app"`
-	VotingPower           float64   `json:"vp"`
-	VotingPowerState      string    `json:"vp_state"`
-	VotingPowerByStrategy []float32 `json:"vp_by_strategy"`
+	ID                    string          `json:"id"`
+	Ipfs                  string          `json:"ipfs"`
+	Voter                 string          `json:"voter"`
+	DaoID                 uuid.UUID       `json:"dao_id"`
+	ProposalID            string          `json:"proposal_id"`
+	Choice                json.RawMessage `json:"choice"`
+	Created               uint64          `json:"created"`
+	Reason                string          `json:"reason"`
+	App                   string          `json:"app"`
+	VotingPower           float64         `json:"vp"`
+	VotingPowerState      string          `json:"vp_state"`
+	VotingPowerByStrategy []float32       `json:"vp_by_strategy"`
 }
