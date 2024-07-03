140ad18fe870c800c56e2b55f36e527ff15f4d23
Alexander Kirii <nwn.wert@gmail.com>
2024-07-04 00:40:12 +0500

    Support delegates API

--- dao/dao.go
+++ dao/dao.go
@@ -82,3 +82,19 @@ type Recommendation struct {
 }
 
 type Recommendations []Recommendation
+
+type Delegate struct {
+	Address                  string  `json:"address"`
+	DelegatorCount           int32   `json:"delegator_count"`
+	PercentOfDelegators      int32   `json:"percent_of_delegators"`
+	VotingPower              float64 `json:"voting_power"`
+	PercentOfVotingPower     int32   `json:"percent_of_voting_power"`
+	About                    string  `json:"about"`
+	Statement                string  `json:"statement"`
+	UserDelegatedVotingPower float64 `json:"user_delegated_voting_power"`
+	VotesCount               int32   `json:"votes_count"`
+	ProposalsCount           int32   `json:"proposals_count"`
+	CreateProposalsCount     int32   `json:"create_proposals_count"`
+}
+
+type Delegates []Delegate
