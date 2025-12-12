29b5b0e2567af8742f3d20e79d1ac4301281d703
Dmitry <dmitry@goverland.xyz>
2025-12-12 13:37:26 +0300

    Fix getting top delegators

--- client_delegate_v2.go
+++ client_delegate_v2.go
@@ -72,7 +72,7 @@ func (c *Client) GetUserTopDelegatorsByDaoV2(
 	address string,
 	daoID uuid.UUID,
 ) (delegate.GetUserDelegatorsTopV2Response, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/daos/%s/delegates/%s/delegators", c.baseURL, daoID.String(), address), nil)
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/daos/%s/delegates/%s/delegators/top", c.baseURL, daoID.String(), address), nil)
 	if err != nil {
 		return delegate.GetUserDelegatorsTopV2Response{}, err
 	}
